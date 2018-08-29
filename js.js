302e4b9464614d0e785944e1332b73fec7ae2c0a
olegmikadze10 <olegmikadze10@gmail.com>
2018-08-29 11:10:59 +0300

    3 sections

---
+++ js.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function () {
+    particlesJS.load('particles-js', 'particles.json', function() {
+        console.log('particles.js loaded - callback');
+    });
+    particlesJS.load('courses', 'particles.json', function() {
+        console.log('particles.js loaded - callback');
+    });
+});
+
