580fc6ca834e0a81119d1ef2a40c40ef2f78b7e9
olegmikadze <olegmikadze10@gmail.com>
2018-12-21 15:10:12 +0200

    burger dropdown menu added

--- js.js
+++ js.js
@@ -13,3 +13,20 @@ jQuery(document).ready(function () {
     });
 });
 
+function myFunction() {
+    document.getElementById("Dropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.portHeader__div__sandwich')) {
+      var dropdowns = document.getElementsByClassName("portHeader__div__list");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
